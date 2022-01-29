a3b8bc769124cfc6ec6c221672657bcc7422d476
Hemant Kumar Sahu <hemantjava90@gmail.com>
2022-01-29 14:19:01 +0530

    Add files via upload

---
+++ src/main/java/com/example/springdatapartial/entity/Person.java
@@ -0,0 +1,27 @@
+package com.example.springdatapartial.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Person {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Integer id;
+   private String firstName;
+   private String lastName;
+   private String email;
+   private String gender;
+   private Integer age;
+
+}
